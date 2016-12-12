7cec7335e04a8fa713697fbce2a1f970e8858cc9
Mohit Kumar <gargmohit67@gmail.com>
2016-12-12 17:44:57 +0530

    bug fix reverted

--- app/src/main/java/com/learning/novoinvent/wherewasme/Activites/HomeScreenMain.java
+++ app/src/main/java/com/learning/novoinvent/wherewasme/Activites/HomeScreenMain.java
@@ -28,6 +28,7 @@ public class HomeScreenMain extends Activity implements View.OnClickListener {
         llSaveNewData = (LinearLayout) findViewById(R.id.llSaveNewData);
         tvAddNewData = (TextView) findViewById(R.id.tvAddNewData);
         tvSeeSavedData = (TextView) findViewById(R.id.tvSeeSavedData);
+
         // On Start Add New Data will be selected by default
         llAddNewData.setBackgroundColor(ContextCompat.getColor(this, R.color.white));
         tvAddNewData.setTextColor(ContextCompat.getColor(this, R.color.purple));
@@ -46,6 +47,7 @@ public class HomeScreenMain extends Activity implements View.OnClickListener {
                 tvAddNewData.setTextColor(ContextCompat.getColor(this, R.color.purple));
                 llSaveNewData.setBackgroundColor(ContextCompat.getColor(this, R.color.purple));
                 tvSeeSavedData.setTextColor(ContextCompat.getColor(this, R.color.white));
+
 
                 break;
             case R.id.llSaveNewData:
--- read.md
+++ read.md
@@ -1,2 +1,1 @@
 # Read Me
-bug - Fixed
